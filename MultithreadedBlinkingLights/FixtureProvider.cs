4d0aebab9f2cbbcb44b8de54af5817ce6c16e70c
Alex Reeser <areeser@corebridge.net>
2020-12-18 10:29:09 -0600

    Provide light through fixtures interface
    now lights are properly decoupled

--- MultithreadedBlinkingLights/FixtureProvider.cs
+++ MultithreadedBlinkingLights/FixtureProvider.cs
@@ -8,21 +8,61 @@ namespace MultithreadedBlinkingLights
     public class FixtureProvider : IFixtureProvider
     {
         readonly ILightHardwareSource _hardware;
+        readonly IServiceProvider _serviceProvider;
+        readonly IPowerSupply _powerSupply;
+
+        private List<ILightEmittingFixture> fixtures = new List<ILightEmittingFixture>();
 
         public FixtureProvider(
-            ILightHardwareSource hardware)
+            ILightHardwareSource hardware,
+            IServiceProvider serviceProvider,
+            IPowerSupply powerSupply
+            )
         {
             _hardware = hardware;
+            _serviceProvider = serviceProvider;
+            _powerSupply = powerSupply;
         }
 
         public void Start()
         {
-            Parallel.For(0, _hardware.GetNumberOfAvailableDiodes(), async (int i) => await BeginIlluminating(i));
+            int numOfDiodes = _hardware.GetNumberOfAvailableDiodes();
+            fixtures = InitializeFixtures(numOfDiodes);
+            Parallel.For(0, numOfDiodes, async (int fixtureIndex) => await IlluminateUntilPowerGoesOut(fixtureIndex));
         }
 
-        public async Task BeginIlluminating(int i)
+        private List<ILightEmittingFixture> InitializeFixtures(int numOfDiodes)
         {
-            await Task.Delay(10);
+            var result = new List<ILightEmittingFixture>();
+            for (int i = 0; i < numOfDiodes; i++)
+            {
+                result.Add(
+                    _serviceProvider.GetService(typeof(ILightEmittingFixture)) as ILightEmittingFixture
+                    );
+            }
+            return result;
+        }
+
+        public async Task IlluminateUntilPowerGoesOut(int fixtureIndex)
+        {
+            ILightEmittingFixture fixture = fixtures[fixtureIndex];
+
+            while (_powerSupply.IsOn && !_hardware.IsBurnOut(fixtureIndex))
+            {
+                CurrentAndWavelength x = await fixture.GetCurrentAndWavelength(fixtureIndex);
+                
+                if (x.NormalizedCurrent > 0)
+                {
+                    _hardware.IncreaseAmperage(fixtureIndex, x.Wavelength);
+                }
+                else
+                {
+                    _hardware.ReduceAmperage(fixtureIndex);
+                }
+
+                await Task.Delay(CONSTANTS.PERCEPTUAL_PHOTON_THRESHOLD_MS);
+
+            }
         }
     }
 }
--- MultithreadedBlinkingLights/ILLUMINATION_CONSTANTS.cs
+++ MultithreadedBlinkingLights/ILLUMINATION_CONSTANTS.cs
@@ -7,5 +7,6 @@ namespace MultithreadedBlinkingLights
     public class CONSTANTS
     {
         public const int NUM_AVAIL_DIODES_TEST = 12;
+        public const int PERCEPTUAL_PHOTON_THRESHOLD_MS = 100;
     }
 }
